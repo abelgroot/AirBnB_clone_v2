97722315027daaaeb008ad75cbdb3d5554f12a5f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-01-06 05:09:12 -0800

    feat;models/user.py: add User model with DBStorage support and required attributes __tablename__, email, password,first_name, last_name

--- models/user.py
+++ models/user.py
@@ -10,8 +10,8 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     def __str__(self):
         dict_copy = self.__dict__.copy()
         dict_copy.pop('_sa_instance_state', None)
