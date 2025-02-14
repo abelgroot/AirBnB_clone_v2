82be8937a5c3a2d419f7dc4951f587d7ede1eb0c
groot <abel.mulugeta.dagne@gmail.com>
2025-02-14 23:00:04 +0300

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder.
+Fabric script that generates a .tgz archive
+from the contents of the web_static folder.
 """
 
 from fabric.api import local
 from datetime import datetime
 import os
+
 
 def do_pack():
     """
