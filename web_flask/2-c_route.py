d951da64ac786124292f6b95a06156a9d924193f
abel mulugeta <abel.mulugeta.dagne@gmail.com>
2025-02-18 21:50:00 +0300

    web_flask/2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application that displays:
+- "Hello HBNB!" on the root route (/)
+- "HBNB" on the /hbnb route
+- "C <text>" on the /c/<text> route, replacing underscores with spaces.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when the root route is accessed.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" when the /hbnb route is accessed.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays "C <text>", replacing underscores with spaces.
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
